dd5555123b09f027c4320e295f02abe809965c23
gaaaaaaabriel <gg405246@gmail.com>
2025-07-15 15:33:22 -0300

    feat: commit inicial do projeto Spring Boot portaria

---
+++ src/test/java/com/gabriel/portaria/PortariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.portaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
