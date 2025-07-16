a19d626306b5b31e99ee86f8ba9d3b51d20fe786
gaaaaaaabriel <gg405246@gmail.com>
2025-07-15 21:20:40 -0300

    feat: criação da interface VeiculoRepository com métodos findByPlaca e existsByPlaca

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 criação do projeto -> https://start.spring.io/
 
 rodar a API -> .\gradlew.bat bootRun
+
+metodos prontos (CRUD) -> JpaRepository
---
+++ src/main/java/com/gabriel/portaria/repository/VeiculoRepository.java
@@ -0,0 +1,12 @@
+package com.gabriel.portaria.repository;
+
+import com.gabriel.portaria.model.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+
+    Veiculo findByPlaca(String placa);
+
+    boolean existsByPlaca(String placa);
+
+}
