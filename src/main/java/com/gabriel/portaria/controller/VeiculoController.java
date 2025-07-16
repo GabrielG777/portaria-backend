299c43b3975064e2b1165757aaf7ff4e02793db4
gaaaaaaabriel <gg405246@gmail.com>
2025-07-16 20:36:49 -0300

    feat: função nova para buscar por placa - buscarPlaca

--- src/main/java/com/gabriel/portaria/controller/VeiculoController.java
+++ src/main/java/com/gabriel/portaria/controller/VeiculoController.java
@@ -1,17 +1,22 @@
 package com.gabriel.portaria.controller;
 
-import com.gabriel.portaria.model.Veiculo;
-import com.gabriel.portaria.repository.VeiculoRepository;
-import com.gabriel.portaria.dto.ErroResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabriel.portaria.dto.ErroResponse;
+import com.gabriel.portaria.model.Veiculo;
+import com.gabriel.portaria.repository.VeiculoRepository;
+
 @RestController
-@RequestMapping("/veiculos") // nome da minha classe, quando eu for acessar a rota, vai ser
-                             // http//:localhost/veiculos
+@RequestMapping("/veiculos") // nome da minha classe, quando eu for acessar a rota, vai ser // http//:localhost/veiculos
 
 public class VeiculoController {
 
@@ -32,6 +37,15 @@ public class VeiculoController {
         veiculo.setStatus(com.gabriel.portaria.model.StatusVeiculo.NO_PATIO);
         Veiculo salvo = veiculoRepository.save(veiculo);
         return ResponseEntity.ok(salvo);
+    }
+
+    @GetMapping("/placa/{placa}") // http://localhost:8080/veiculos/placa/
+    public ResponseEntity<?> buscarPlaca(@PathVariable String placa) {
+        Veiculo veiculo = veiculoRepository.findByPlaca(placa);
+        if (veiculo == null) {
+            return ResponseEntity.status(404).body(new ErroResponse("Veículo não encontrado"));
+        }
+        return ResponseEntity.ok(veiculo);
     }
 
 }
