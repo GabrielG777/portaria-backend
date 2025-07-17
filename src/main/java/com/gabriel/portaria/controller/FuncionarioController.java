b9a497077e05403517e2d842e37ba6f622d5c473
gaaaaaaabriel <gg405246@gmail.com>
2025-07-16 22:17:43 -0300

    Feat: Função para criar atualizar funcionario criada - atualizarFuncionario()

--- src/main/java/com/gabriel/portaria/controller/FuncionarioController.java
+++ src/main/java/com/gabriel/portaria/controller/FuncionarioController.java
@@ -2,6 +2,7 @@ package com.gabriel.portaria.controller;
 
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,9 @@ import com.gabriel.portaria.model.Funcionario;
 import com.gabriel.portaria.repository.FuncionarioRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +38,25 @@ public class FuncionarioController {
             return ResponseEntity.status(409).body(new ErroResponse("Funcionário com esse CPF já existe"));
         }
         return ResponseEntity.ok(funcionarioRepository.save(funcionario));
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<?> atualizarFuncionario(@PathVariable Long id, @RequestBody Map<String, Object> updates) {
+        return funcionarioRepository.findById(id).<ResponseEntity<?>>map(funcionario -> {
+            try {
+                updates.forEach((chave, valor) -> {
+                    switch (chave) {
+                        case "nome" -> funcionario.setNome((String) valor);
+                        case "cargo" -> funcionario.setCargo((String) valor);
+                        case "cpf" -> funcionario.setCpf((String) valor);
+                    }
+                });
+
+                return ResponseEntity.ok(funcionarioRepository.save(funcionario));
+            } catch (Exception e) {
+                return ResponseEntity.badRequest().body(new ErroResponse("Erro ao atualizar funcionário"));
+            }
+        }).orElse(ResponseEntity.status(404).body(new ErroResponse("Funcionário não encontrado")));
     }
 
 }
