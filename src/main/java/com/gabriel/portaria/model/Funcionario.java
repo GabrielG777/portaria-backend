c8145a9e19a4020574895f78fcb6779826f367d7
gaaaaaaabriel <gg405246@gmail.com>
2025-07-16 21:57:06 -0300

    Feat: Model de funcionario

--- src/main/java/com/gabriel/portaria/model/Funcionario.java
+++ src/main/java/com/gabriel/portaria/model/Funcionario.java
@@ -1,2 +1,49 @@
-// Modelo: id, nome, cnh (Carteira Nacional de Habilitação).
+package com.gabriel.portaria.model;
 
+import jakarta.persistence.*;
+
+@Entity
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String cargo;
+    private String cpf;
+
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
--- src/main/java/com/gabriel/portaria/model/StatusVeiculo.java
+++ src/main/java/com/gabriel/portaria/model/StatusVeiculo.java
@@ -1,5 +1,7 @@
 package com.gabriel.portaria.model;
 
+// esse cara aqui é um enum, que é uma lista de constantes
+// uso ele para definir os status possíveis de um veículo
 public enum StatusVeiculo {
     NO_PATIO,
     EM_VIAGEM
