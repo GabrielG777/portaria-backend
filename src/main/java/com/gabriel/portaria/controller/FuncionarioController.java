d6af5f020222991da2369be2863c109179db6ffc
gaaaaaaabriel <gg405246@gmail.com>
2025-07-16 22:06:53 -0300

    Feat: FuncionarioController criado, com a função de listar os funcionarios

--- src/main/java/com/gabriel/portaria/controller/FuncionarioController.java
+++ src/main/java/com/gabriel/portaria/controller/FuncionarioController.java
@@ -1,5 +1,21 @@
 package com.gabriel.portaria.controller;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.gabriel.portaria.model.Funcionario;
+import com.gabriel.portaria.repository.FuncionarioRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class FuncionarioController {
-    
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    @GetMapping
+    public List<Funcionario> listarFuncionarios(){
+        return funcionarioRepository.findAll();
+    }
+
 }
